46b99cae40bd834e970dcec0f5421bf88b7f400c
Xiongbo Cen <xiongbo.cen@prudential.com.cn>
2021-03-24 11:04:45 +0800

    add navigator header and readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # react-navigation-custom-header-example
+
+## library
+```json
+"react-navigation": "^4.4.0",
+"react-navigation-stack": "^2.8.2",
+```
+## 整个 stack 级别的自定义header, 放在 defaultNavigationOptions
+```ts
+defaultNavigationOptions:{
+  header:<View><Text>default custom header</Text></View>
+}
+```
+
+## 页面级别的自定义header, 放在 navigationOptions, 会覆盖stack的header
+```ts
+navigationOptions:{
+  header:<View><Text>screen custom header</Text></View>
+}
+```
+## 注：例子只包含 navigator 的设置，RN项目代码，screen页面需要自己定义
---
+++ RootNavigator.js
@@ -0,0 +1,32 @@
+import {View, Text} from 'react-native'
+import React from 'react'
+
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
+import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
+import TestScreen from './screens/TestScreen';
+
+const RootNavigator = createStackNavigator(
+  {
+    Home: {screen: HomeScreen},
+    Login: {screen: LoginScreen},
+    Test: {
+      screen: TestScreen,
+      // 页面级别的自定义header, 放在navigationOptions, 会覆盖全局的header
+      navigationOptions:{
+        header:<View><Text>screen custom header</Text></View>
+      }
+    },
+  },
+  {
+    initialRouteName: 'Test',
+    // 整个 stack 级别的自定义header, 放在navigationOptions
+    defaultNavigationOptions:{
+      header:<View><Text>default custom header</Text></View>
+    }
+  },
+);
+
+export default createAppContainer(RootNavigator);
